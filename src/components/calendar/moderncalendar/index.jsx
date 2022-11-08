d019164b8763c3c1f33193c8929d1373cdd31549
Coligar Sistemas <acorrea@luxfacta.com>
2022-11-07 21:08:41 -0300

    Filtro de datas

--- src/components/calendar/moderncalendar/index.jsx
+++ src/components/calendar/moderncalendar/index.jsx
@@ -6,6 +6,13 @@ import Button from '@mui/material/Button'
 import Pin from './pin/pin'
 import Calendar from './calendar'
 
+
+import TextField from '@mui/material/TextField';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+
 import Person from '@mui/icons-material/Person'
 import CalendarIcon from '@mui/icons-material/Today'
 
@@ -15,16 +22,52 @@ const ModernCalendar = (props) =>
 {
     const [searchTerm, setSearchTerm] = useState({type:'', term:''})
     const [scheduleList, setScheduleList] = useState([])
+    const [schedule, setSchedule] = useState([])
+    const [dataInterval, setDataInterval] = useState()
+    const [dateValue, setDateValue] = useState(null)
     
     var current = new Date()
 
-    /*let last_day = new Date(current.getTime() - 86400000).toLocaleDateString()
-    let current_day = new Date().toLocaleDateString()
-    let next_day = new Date(current.getTime() + 86400000).toLocaleDateString()*/
+    const getScheduleInterval = (days = 0, startDate = '') =>
+    {
+        let period = addSubDays(days)
+        let title = ''
+
+        let date = new Date()
+        let current_day = date.setDate(date.getDate());
+
+        if(days==0 && startDate != '')
+        {
+            period = new Date(startDate)
+        }
+
+        let filtered_data = schedule.filter((element, index) => 
+            new Date(element.day).toLocaleDateString() >= new Date(current_day).toLocaleDateString() && 
+            new Date(element.day).toLocaleDateString() <= period.toLocaleDateString()
+        )
+
+        let filtered_schedule =[
+            {
+                date_id: 1,
+                date: `${new Date(current_day).toLocaleDateString()} até o dia ${period.toLocaleDateString()}`,
+                data: filtered_data,
+            }
+        ]
+
+        setScheduleList(filtered_schedule);
+    }
+
+    const addSubDays = Date.prototype.addDays = (days = 0) =>
+    {
+        let date = new Date()
+        date.setDate(date.getDate() + days)
+        return date
+    }
 
     let last_day = new Date('2022/01/01').toLocaleDateString()
     let current_day = new Date('2022/01/02').toLocaleDateString()
     let next_day = new Date('2022/01/03').toLocaleDateString()
+    let week = new Date('2022/01/21')
 
     console.log('dias oroginal :',last_day, current_day, next_day)
 
@@ -36,12 +79,14 @@ const ModernCalendar = (props) =>
           .then((response) => {
             return response.json()
           }) 
-          .then((json) => {
-            console.log(json)
+          .then((json) => 
+          {
             const yesterday = json.filter((element, index) => new Date(element.day).toLocaleDateString('pt-BR') === last_day)
             const current = json.filter((element, index) => new Date(element.day).toLocaleDateString('pt-BR') === current_day)
             const tomorrow = json.filter((element, index) => new Date(element.day).toLocaleDateString('pt-BR') === next_day)
-            console.log('dias retorno :',yesterday, current, tomorrow)
+
+            setSchedule(json)
+
             const schedule =[
                 {
                     date_id: 1,
@@ -67,7 +112,7 @@ const ModernCalendar = (props) =>
         {
             console.log(error)
         }
-    },[])
+    },[current_day, last_day, next_day])
 
 
     const doSchedule = useCallback(async () =>
@@ -103,7 +148,7 @@ const ModernCalendar = (props) =>
         {
             console.log(error)
         }
-    },[current_day])
+    },[])
 
 
     useEffect(() => 
@@ -112,174 +157,7 @@ const ModernCalendar = (props) =>
         getData()
     },[doSchedule, getData])
 
-    /**Array com o resultado da consulta ao banco de dados para agenda
-     * Todas as formas de pesquisa (dia, semana, mês e intervalo de datas)
-     * passam por essa consulta e o resultado formatar igual array abaixo. 
-     * Implementar método
-     */
-    const schedule =[
-        {
-            date_id: 1,
-            date: last_day,
-            data: [],
-        },
-        {
-            date_id: 2,
-            date: current_day,
-            data:[ 
-                {
-                    id: '7',
-                    order: '1',
-                    name: 'Alexandre Henrique',
-                    interviewer: 'TI | Diego Camargo',
-                    day: current_day,
-                    starttime: '08:00',
-                    endtime: '09:00',
-                    area: 'rh',
-                    type: 'agendamentosolicitado',
-                    avatar: 'https://observatoriodocinema.uol.com.br/wp-content/uploads/2018/08/cropped-exterminador-do-futuro-6-arnold-schwarzenegger-prepara-novo-ator-3.jpg'
-                },
-                {
-                    id: '8',
-                    order: '2',
-                    name: 'José Maria',
-                    interviewer: 'Administrativo | Elisângela',
-                    day: current_day,
-                    starttime: '09:30',
-                    endtime: '10:30',
-                    area: 'administrativo',
-                    type: 'reagendamentosolicitado',
-                    avatar: 'https://spinoff.com.br/wp-content/uploads/o-exterminador-do-futuro-1.jpg'
-                }, 
-                {
-                    id: '9',
-                    order: '3',
-                    name: 'Andréia Marques',
-                    interviewer: 'Comercial | Elisângela',
-                    day: current_day,
-                    starttime: '11:00',
-                    endtime: '12:30',
-                    area: 'comercial',
-                    type: '',
-                    avatar: 'https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img,w_750,h_375/https://poltronanerd.com.br/wp-content/uploads/2020/05/jake-neyriti-james-cameron-avatar-2-capa-minhaseriefavorita-1280x720-1-750x375.jpg'
-                },
-                {
-                    id: '10',
-                    order: '4',
-                    name: 'Marcelo Bombonato',
-                    interviewer: 'Representante | Elisângela',
-                    day: current_day,
-                    starttime: '13:00',
-                    endtime: '15:00',
-                    area: 'representante',
-                    type: '',
-                    avatar: 'https://www.thewrap.com/wp-content/uploads/2016/05/Top-Gun-Tom-Cruise.png'
-                },
-                {
-                    id: '11',
-                    order: '5',
-                    name: 'Zélia Cardoso',
-                    interviewer: 'Produção | Elisângela',
-                    day: current_day,
-                    starttime: '15:15',
-                    endtime: '16:30',
-                    area: 'produção',
-                    type: '',
-                    avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZOg5mRt2Y77bTrMb6iyUhBbXY5ErS_QOL5Q&usqp=CAU'
-                },
-                {
-                    id: '12',
-                    order: '6',
-                    name: 'Luis Skagion',
-                    interviewer: 'Comercial | Elisângela',
-                    day: current_day,
-                    starttime: '17:00',
-                    endtime: '18:30',
-                    area: 'comercial',
-                    type: '',
-                    avatar: 'https://tm.ibxk.com.br/2014/06/24/24104456847152.jpg?ims=1120x420'
-                }
-            ],
-        },
-        {
-            date_id: 3,
-            date: next_day,
-            data:[ 
-                {
-                    id: '13',
-                    order: '1',
-                    name: 'José Maciel',
-                    interviewer: 'TI | Diego Camargo',
-                    day: next_day,
-                    starttime: '08:00',
-                    endtime: '09:00',
-                    area: 'rh',
-                    type: 'agendamentosolicitado',
-                    avatar: 'https://smartgreen.net/wp-content/uploads/2021/01/person.jpg'
-                },
-                {
-                    id: '14',
-                    order: '2',
-                    name: 'Kátia Maria',
-                    interviewer: 'Administrativo | Elisângela',
-                    day: next_day,
-                    starttime: '09:30',
-                    endtime: '10:30',
-                    area: 'administrativo',
-                    type: 'reagendamentosolicitado',
-                    avatar: 'https://portal-assets.icnetworks.org/uploads/picture/file/104874/resized_045.DomingasPerson_Oc.Person_IC_FotoAndreSeiti3.jpg'
-                }, 
-                {
-                    id: '15',
-                    order: '3',
-                    name: 'Marina Silva',
-                    interviewer: 'Comercial | Elisângela',
-                    day: next_day,
-                    starttime: '11:00',
-                    endtime: '12:30',
-                    area: 'comercial',
-                    type: '',
-                    avatar: 'https://br.web.img3.acsta.net/pictures/15/09/01/18/54/060755.jpg'
-                },
-                {
-                    id: '16',
-                    order: '4',
-                    name: 'Flávio Zanão',
-                    interviewer: 'Representante | Elisângela',
-                    day: next_day,
-                    starttime: '13:00',
-                    endtime: '14:00',
-                    area: 'representante',
-                    type: '',
-                    avatar: 'https://m0.her.ie/wp-content/uploads/2018/01/07093633/GettyImages-887815620.jpg'
-                },
-                {
-                    id: '17',
-                    order: '5',
-                    name: 'Oliver Bituso',
-                    interviewer: 'Produção | Elisângela',
-                    day: next_day,
-                    starttime: '14:55',
-                    endtime: '16:30',
-                    area: 'produção',
-                    type: '',
-                    avatar: 'https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F61688aa1d4a8658c3f4d8640%2FAntonio-Juliano%2F0x0.jpg%3Ffit%3Dscale'
-                },
-                {
-                    id: '18',
-                    order: '6',
-                    name: 'Maiara Azevedo',
-                    interviewer: 'Comercial | Elisângela',
-                    day: next_day,
-                    starttime: '17:00',
-                    endtime: '18:00',
-                    area: 'comercial',
-                    type: '',
-                    avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_ocW9x-t2gGJPKg3iqvoEfm7qJSB9ujqnqA&usqp=CAU'
-                }
-            ],
-        }
-    ]
+
 
     const schedule_future = [
        
@@ -481,26 +359,31 @@ const ModernCalendar = (props) =>
 
                             <Paper
                                 component="form"
-                                sx={{ p: '4px', display: 'flex', alignItems: 'center', width: '48px', color: 'white', backgroundColor: '#1592E6', fontWeight: '500', textAlign: 'center', justifyContent: 'center', margin:'0 1px 0 0'}}
                                 elevation= {1}
+                                onClick={() => getScheduleInterval()}
+                                className={style.schedule_buttons}
+                                sx={{ padding: '0px', margin:'0px'}}
                             >
-                                dia
+                               <span className={style.schedule_buttons}>Dia</span>
                             </Paper>
 
                             <Paper
                                 component="form"
-                                sx={{ p: '4px', display: 'flex', alignItems: 'center', width: '76px', color: 'black', backgroundColor: 'white', fontWeight: '500', textAlign: 'center', justifyContent: 'center', margin:'0 1px 0 0'}}
+                                className={style.schedule_buttons}
                                 elevation= {1}
+                                onClick={() => getScheduleInterval(7)}
                             >
-                                semana
+                                <span className={style.schedule_buttons}>Semana</span>
                             </Paper>
 
                             <Paper
                                 component="form"
-                                sx={{ p: '4px', display: 'flex', alignItems: 'center', width: '51px', color: 'black', backgroundColor: 'white', fontWeight: '500', textAlign: 'center', justifyContent: 'center', margin:'0 15px 0 0'}}
+                                className={style.schedule_buttons}
+                                sx={{ p: '0px'}}
                                 elevation= {1}
+                                onClick={() => getScheduleInterval(30)}
                             >
-                                mês
+                              <span className={style.schedule_buttons}>Mês</span>
                             </Paper>
 
                             <Paper
@@ -508,7 +391,17 @@ const ModernCalendar = (props) =>
                                 sx={{ p: '4px', display: 'flex', alignItems: 'center', width: '191px', color: '#5A607F', backgroundColor: 'white', fontWeight: '400', textAlign: 'center', justifyContent: 'center',  margin:'0'}}
                                 elevation= {1}
                             >
-                                <CalendarIcon/> Jan, 2021 - Dez, 2021
+                                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                    <DatePicker
+                                        label="Data"
+                                        value={dateValue}
+                                        onChange={(newValue) => {
+                                            setDateValue(newValue);
+                                            getScheduleInterval(0, newValue);
+                                        }}
+                                        renderInput={(params) => <TextField {...params} />}
+                                    />
+                                </LocalizationProvider>
                             </Paper>
 
                         </div>
@@ -518,7 +411,9 @@ const ModernCalendar = (props) =>
                                 border: '2px solid currentColor',
                                 backgroundColor:'#1592E6',
                                 color: 'white'
-                            }, margin:'10px 0 0 0'}}>
+                            }, margin:'10px 0 0 0'}}
+                            onClick={()=> getData()}
+                        >
                                 ver agenda
                         </Button>
 
